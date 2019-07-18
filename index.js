d5070e5ba4e24fe18f28a939c28657a0d656b2b0
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2019-07-18 18:12:40 +0100

    Import readline to handle input

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("App");
+// Import readline for getting input from the command line
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+
+// Get IP and return it to the user
+readline.question("Enter an IP Address: ", (ipAddress) => {
+    console.log(`Your IP Address was ${ipAddress}`);
+    process.exit();
+});
+
